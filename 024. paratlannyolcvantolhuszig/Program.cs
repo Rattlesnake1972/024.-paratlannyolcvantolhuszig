c3eb0cb42433468ad93d810e1b5533708ca9d605
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 16:22:47 +0200

    80-tól 20-ig a páratlan számok

---
+++ 024. paratlannyolcvantolhuszig/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024.paratlannyolcvantolhuszig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 80; i > 19; i--)
+
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            Console.ReadKey();
+        }
+    }
+}
